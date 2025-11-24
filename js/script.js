a958bb3ed4d1ced5700fdfa8efee0a4cf394138d
joseg789 <joseg191.js@gmail.com>
2025-11-24 21:26:04 +0100

    srefactoring

--- js/script.js
+++ js/script.js
@@ -1,55 +1,57 @@
 let countId = document.getElementById("countdown");
 const userInput = document.getElementById("userInput");
 const result = document.getElementById("result");
-let p = document.createElement("p");
-let res = document.createElement("p");
 const restart = document.getElementById("restart");
 
 const getNumber = () => {
   return Math.floor(Math.random() * 3 + 1);
 };
 
+const mostrarResultados = (win, numberAll) => {
+  if (win) {
+    result.innerHTML = `<p class="green">HAS SALVADO AL MUNDO 👑</p>
+    <p>Tu numero ${+userInput.value} es igual a ${numberAll}</p>`;
+  } else {
+    result.innerHTML = `<p class="red">BOOM!!💣💣💣</p>
+    <p>Tu numero ${+userInput.value} es diferente a ${numberAll}</p>`;
+  }
+};
+
+const startGame = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(getNumber()); // resolvemos cuando termina el conteo
+    }, 6000);
+  });
+};
+//events
 userInput.addEventListener("change", () => {
-  res.textContent = "";
+  result.innerHTML = "";
   let count = 6; // segundos iniciales
   userInput.disabled = true;
   countId.classList.remove("count");
-  const promesa = new Promise((resolve) => {
-    setTimeout(() => {
-      const interval = setInterval(() => {
-        count--;
-        countId.textContent = `cuenta atras: ${count}`;
 
-        if (count === 0) {
-          clearInterval(interval); //para que no se vuelva a llamar el intervalo
-          resolve(getNumber());
-          countId.classList.add("count");
-        }
-      }, 1000);
-      p.innerHTML = "";
-    });
-  }, 5000);
+  const interval = setInterval(() => {
+    count--;
+    countId.textContent = `cuenta atrás: ${count}`;
 
-  promesa.then((numberAll) => {
+    if (count === 0) {
+      clearInterval(interval);
+      countId.classList.add("count");
+    }
+  }, 1000);
+
+  startGame().then((numberAll) => {
     if (numberAll === +userInput.value) {
-      p.textContent = "HAS SALVADO AL MUNDO 👑";
-      p.classList.add("green");
-      p.classList.remove("red");
-      result.appendChild(p);
-      res.textContent = `Tu numero ${+userInput.value} es igual a ${numberAll} `;
-      result.appendChild(res);
+      mostrarResultados(true, numberAll);
     } else {
-      p.textContent = "BOOM!! 💣💣💣";
-      p.classList.add("red");
-      p.classList.remove("green");
-      res.textContent = `Tu numero ${+userInput.value} es diferente a ${numberAll} `;
-      result.appendChild(p);
-      result.appendChild(res);
+      mostrarResultados(false, numberAll);
     }
     userInput.disabled = false;
   });
+  countId.innerHTML = "";
+});
 
-  restart.addEventListener("click", () => {
-    location.reload();
-  });
+restart.addEventListener("click", () => {
+  location.reload();
 });
